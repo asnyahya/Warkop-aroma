fa57db9c3b23572da59e4b56f14469ae59376d80
asnyahya <mohasnawi545@gmail.com>
2024-07-26 16:41:26 +0700

    Membuat bagian about dan memperbaiki hamburger menu

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@ const hamburgerMenu = document.querySelector('#hamburger-menu');
 const navbarNav = document.querySelector('.navbar .navbar-item');
 
 hamburgerMenu.addEventListener('click', function (e) {
-  e.preventDefault();
+  e.preventDefault(); // Mencegah tindakan default browser
   navbarNav.classList.toggle('active');
+});
+
+// Menambahkan event click ke seluruh dokumen untuk menutup hamburger menu di luar manapun
+document.addEventListener('click', function (e) {
+  if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
 });
