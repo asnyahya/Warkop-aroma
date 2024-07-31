49e7168dede49f9e0578d2748d1f960252033592
asnyahya <mohasnawi545@gmail.com>
2024-07-31 23:03:53 +0700

    Membuat search form

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
 const hamburgerMenu = document.querySelector('#hamburger-menu');
 const navbarNav = document.querySelector('.navbar .navbar-item');
+const searchBtn = document.querySelector('#search-button');
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
 
+// Event click untuk hamburger menu
 hamburgerMenu.addEventListener('click', function (e) {
   e.preventDefault(); // Mencegah tindakan default browser
   navbarNav.classList.toggle('active');
 });
 
-// Menambahkan event click ke seluruh dokumen untuk menutup hamburger menu di luar manapun
+// Event click untuk search form
+searchBtn.addEventListener('click', (e) => {
+  searchForm.classList.toggle('show');
+  searchBox.focus();
+  e.preventDefault();
+});
+
+// Menambahkan event click ke seluruh dokumen untuk pengkondisian
 document.addEventListener('click', function (e) {
+  // Kondisi dimana hamburger menu di tutup di luar bagian navigation
   if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
+  }
+  // Kondisi untuk search
+  if (!searchBtn.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.toggle('show');
   }
 });
